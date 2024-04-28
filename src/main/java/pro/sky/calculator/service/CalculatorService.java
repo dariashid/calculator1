996347e990f2e21de7267c771312cddef0c956f2
dariashid <brbrbrbrbrbrbr@internet.ru>
2024-04-28 23:13:41 +0300

    calculator

--- src/main/java/pro/sky/calculator/service/CalculatorService.java
+++ src/main/java/pro/sky/calculator/service/CalculatorService.java
@@ -1,11 +1,11 @@
 package pro.sky.calculator.service;
 
 public interface CalculatorService {
-    int sum (int num1, int num2);
+    int sum(int num1, int num2);
 
-    int minus (int num1, int num2);
+    int minus(int num1, int num2);
 
-    int multiply (int num1, int num2);
+    int multiply(int num1, int num2);
 
-    int divide (int num1, int num2);
+    int divide(int num1, int num2);
 }
--- src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/calculator/service/CalculatorServiceImpl.java
@@ -6,26 +6,26 @@ import pro.sky.calculator.exeption.ZeroDividerException;
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
     @Override
-    public int sum(int nam1, int num2) {
-        return nam1+num2;
+    public int sum(int num1, int num2) {
+        return num1 + num2;
     }
 
     @Override
     public int minus(int num1, int num2) {
-        return num1-num2;
+        return num1 - num2;
     }
 
     @Override
     public int multiply(int num1, int num2) {
-        return num1*num2;
+        return num1 * num2;
     }
 
     @Override
     public int divide(int num1, int num2) {
-        if (num2 == 0){
+        if (num2 == 0) {
             throw new ZeroDividerException();
         }
-        return num1/num2;
+        return num1 / num2;
 
     }
 }
